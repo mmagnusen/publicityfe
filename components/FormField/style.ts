49f0c8a55dcae46962e8ebe550c283b7dc049168
Marilyn Magnusen <m-mags@hotmail.co.uk>
2026-06-25 00:35:59 +0100

    make field labels visible on mobile night mode

--- components/FormField/style.ts
+++ components/FormField/style.ts
@@ -1,7 +1,7 @@
 import { cva } from "class-variance-authority";
 
 export const labelledFieldVariants = cva(
-	"mb-4 grid grid-cols-1 gap-4 [&_label]:flex [&_label]:items-center [&_label]:justify-start",
+	"mb-4 grid grid-cols-1 gap-4 [&_label]:flex [&_label]:items-center [&_label]:justify-start [&_label]:text-black",
 );
 
 export const labelRowVariants = cva("flex gap-4");
--- components/FormikFields/Field/style.ts
+++ components/FormikFields/Field/style.ts
@@ -1,5 +1,5 @@
 import { cva } from "class-variance-authority";
 
 export const fieldVariants = cva(
-	"mb-4 grid grid-cols-1 gap-4 [&_label]:flex [&_label]:items-center [&_label]:justify-start",
+	"mb-4 grid grid-cols-1 gap-4 [&_label]:flex [&_label]:items-center [&_label]:justify-start [&_label]:text-black",
 );
